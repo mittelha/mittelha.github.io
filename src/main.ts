d3d26028a917dfcbc7252ed24a8c4fe49dff0693
mittelha <mitte.lha@gmail.com>
2024-08-28 00:32:08 +0200

    Save

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,11 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+document.onkeydown = (event: KeyboardEvent) => {
+  if (event.key === 'PrintScreen' || event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I')) {
+    event.preventDefault();
+    return false;
+  }
+  return true;
+}
