498b0c34e9da0bb742b26a372a0ba30455db3711
mittelha <mitte.lha@gmail.com>
2024-09-09 23:01:00 +0200

    end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,12 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RulesComponent } from './rules/rules.component';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { SphinxComponent } from './sphinx/sphinx.component';
-import { simpler } from '../main';
 
-interface CapV {
-    v: string;
-}
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, RulesComponent, SphinxComponent],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-    displayRules: boolean = false;
-    ecranEtroit: boolean = false;
-
-    private static d: string =
-        'W3sNCgkidiI6ICJIT05ORVRFVEUiDQp9LCB7DQoJInYiOiAiQVVEQUNFIg0KfSwgew0KCSJ2IjogIkNPTkZJQU5DRSINCn0sIHsNCgkidiI6ICJMSUJFUlRFIg0KfSwgew0KCSJ2IjogIlBMQUlTSVIiDQp9LCB7DQoJInYiOiAiU0lNUExJQ0lURSINCn0sIHsNCgkidiI6ICJTT0xJREFSSVRFIg0KfV0=';
-    private static c: CapV[] = JSON.parse(atob(AppComponent.d)) as CapV[];
-
-    constructor(private responsiveService: BreakpointObserver) {}
-
-    ngOnInit(): void {
-        this.responsiveService
-            .observe([
-                Breakpoints.XSmall,
-                Breakpoints.Small,
-                Breakpoints.Medium,
-                Breakpoints.Large,
-                Breakpoints.XLarge,
-            ])
-            .subscribe((result) => {
-                this.ecranEtroit =
-                    result.breakpoints[Breakpoints.XSmall] ||
-                    result.breakpoints[Breakpoints.Small];
-            });
-    }
-
-    @HostListener('document:contextmenu', ['$event'])
-    onRightClick(event: any) {
-        event.preventDefault();
-    }
-
-    affichageRegles(doitAfficher: boolean) {
-        this.displayRules = doitAfficher;
-    }
-
-    public static correspondance7(
-        lettre_chiffre: string
-    ): string[] | undefined {
-        if (lettre_chiffre?.length == 2) {
-            const c = AppComponent.c.filter(
-                (cv) => cv.v.charAt(0) === lettre_chiffre.charAt(0)
-            );
-            if (c?.length) {
-                return c.map((cv) =>
-                    cv.v.charAt(Number.parseInt(lettre_chiffre.charAt(1)))
-                );
-            }
-        }
-        console.error('Je ne sais pas décoder cela.');
-        return undefined;
-    }
-}
+export class AppComponent { }
--- src/main.ts
+++ src/main.ts
@@ -3,34 +3,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { Md5 } from 'ts-md5';
-
-export const read = (str: string) => '' + new Md5().appendStr(str).end();
-
-export const simpler = (str: string) =>
-    str
-        .trim()
-        .normalize('NFD')
-        .replace(/\p{Diacritic}/gu, '')
-        .toUpperCase();
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
     console.error(err)
 );
-
-document.onkeydown = (event: KeyboardEvent) => {
-    if (
-        event.key === 'PrintScreen' ||
-        event.key === 'F12' ||
-        (event.ctrlKey &&
-            event.shiftKey &&
-            (event.key === 'I' || event.key === 'J'))
-    ) {
-        event.preventDefault();
-        return false;
-    }
-    return true;
-};
-
-(window as any)['correspondance7'] = (str: string) =>
-    AppComponent.correspondance7(str);
